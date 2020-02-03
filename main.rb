cc953edd86fb0c804532ef221e7fce3fd267722e
Pascal <pascalvanleeuwen604@gmail.com>
2020-02-03 23:57:52 +0000

    Added begin/rescue/end block around application to handle Ctrl-C

--- main.rb
+++ main.rb
@@ -1,5 +1,19 @@
 require './game'
 
-puts "Hello and welcome to the game."
+begin
+  puts "Hello and welcome to the game."
 
-game = Game.new
+  puts "Type 'N' for New Game. \nType 'Q' to quit."
+
+  if gets.chomp == 'N'
+    puts "You have selected New Game. Have fun!"
+    game = Game.new
+  else
+    puts "Goodbye see you!"
+    exit
+  end
+  
+rescue Exception => e
+  puts " Goodbye see you!\n"
+  exit
+end
